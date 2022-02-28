1d94dd95c2b006c0dfa79e1d26aa0a2c88bc6ee1
elsa <elizabethamy199@gmail.com>
2022-02-28 14:55:09 -0500

    Added yaw (attempt 1)

--- inverse_kin.py
+++ inverse_kin.py
@@ -1,10 +1,26 @@
+from tkinter import Y
 from ikpy.chain import Chain
 from ikpy.link import OriginLink,URDFLink
 import numpy as np
 import ikpy.utils.plot as plot_utils
 import matplotlib.pyplot
 from mpl_toolkits.mplot3d import Axes3D
+import math
 
+## Input
+coords = [55, 100, -50]
+angle = 20
+
+# Trig angle for link coords
+hypotenuse = 30 ## CHANGE
+temp = hypotenuse*math.cos(math.radians(15)) # predetermined constant angle ## CHANGE 15
+tran_z = hypotenuse*math.sin(math.radians(15))
+tran_x = temp*math.sin(math.radians(angle))
+tran_y = temp*math.cos(math.radians(angle))
+
+print(tran_x,tran_y,tran_z)
+
+## Define Chain
 my_chain = Chain(name='gantry', links=[
     OriginLink(),
     URDFLink(
@@ -27,10 +43,17 @@ my_chain = Chain(name='gantry', links=[
         origin_orientation=[0, 0, 0],
         translation=[0,0,-100],
         joint_type='prismatic'
+    ),
+    URDFLink(
+        name="yaw",
+        origin_translation=[tran_x, tran_y, 0],
+        origin_orientation=[0, 0, 0],
+        rotation=[0,0,0],
+        #joint_type='revolute'
     )
 ])
 
-location = my_chain.inverse_kinematics([55, 100, -50])
+location = my_chain.inverse_kinematics(coords)
 print(location) # Basically outputs percentage of gantry it's gone through
 ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 ax.set_xlabel('x')
