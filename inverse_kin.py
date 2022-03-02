512be20a5109039f1fddacc930e135409c5862af
elsa <elizabethamy199@gmail.com>
2022-03-02 11:05:35 -0500

    Added step count (first attempt)

--- inverse_kin.py
+++ inverse_kin.py
@@ -11,7 +11,16 @@ import math
 coords = [55, 100, -50]
 angle = 20
 
-# Trig angle for link coords
+
+## Variables
+stepsperrev = 200 #200 steps/rev
+distance = 1.27 #mm distance travelled for 1/4-20
+xlength = 150
+ylength = 245
+zlength = 100
+steps = np.zeros((4,1)) #x,y,z,yaw
+
+## Trig angle for link coords
 hypotenuse = 30 ## CHANGE
 temp = hypotenuse*math.cos(math.radians(15)) # predetermined constant angle ## CHANGE 15
 tran_z = hypotenuse*math.sin(math.radians(15))
@@ -27,21 +36,21 @@ my_chain = Chain(name='gantry', links=[
         name="x_gantry",
         origin_translation=[0, 0, 0],
         origin_orientation=[0, 0, 0],
-        translation=[150,0,0],
+        translation=[xlength,0,0],
         joint_type='prismatic'
     ),
     URDFLink(
         name="y_gantry",
         origin_translation=[0, 0, 0],
         origin_orientation=[0, 0, 0],
-        translation=[0,245,0],
+        translation=[0,ylength,0],
         joint_type='prismatic'
     ),
     URDFLink(
         name="z_gantry",
         origin_translation=[0, 0, 0],
         origin_orientation=[0, 0, 0],
-        translation=[0,0,-100],
+        translation=[0,0,-zlength],
         joint_type='prismatic'
     ),
     URDFLink(
@@ -54,10 +63,17 @@ my_chain = Chain(name='gantry', links=[
 ])
 
 location = my_chain.inverse_kinematics(coords)
-print(location) # Basically outputs percentage of gantry it's gone through
+#print(location) # Basically outputs percentage of gantry it's gone through
 ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 my_chain.plot(location, ax)
 matplotlib.pyplot.show()
+
+# number of steps
+steps[0] = (location[1]*xlength)/distance*stepsperrev
+steps[1] = (location[2]*ylength)/distance*stepsperrev
+steps[2] = (location[3]*zlength)/distance*stepsperrev
+steps[3] = angle/1.8
+print(steps)
